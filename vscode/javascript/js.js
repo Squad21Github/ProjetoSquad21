0e355aeee90eec4b1ac85c209e73be476f524fea
andrecosta <andrecosta8224@gmail.com>
2021-12-28 23:00:04 -0300

    corrigindo js

--- vscode/javascript/js.js
+++ vscode/javascript/js.js
@@ -3,12 +3,16 @@ function Tempo() {
     const conteudo = (
         <div>
             <h3>Tempo atual</h3>
-            <p>Estamos na seguinte hora: {new Date().toLocaleTimeString()}</p>/
+            <p>Estamos na seguinte hora: {new Date().toLocaleTimeString()}</p>
         </div>
     )
     ReactDOM.render(conteudo,root)
 }
 
 const root = document.getElementById('root')
+
+root.style.textAlign = "center"
+
+root.style.marginTop = "60px"
 
 setInterval(Tempo, 1000)
