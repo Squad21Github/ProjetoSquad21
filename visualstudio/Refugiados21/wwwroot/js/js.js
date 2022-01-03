df28ed11a65484ce5f90da1d220d85de4468ea76
andrecosta <andrecosta8224@gmail.com>
2022-01-03 13:17:31 -0300

    Atualizei o front

--- visualstudio/Refugiados21/wwwroot/js/js.js
+++ visualstudio/Refugiados21/wwwroot/js/js.js
@@ -1,9 +1,7 @@
 function Tempo() {
-    // jsx a apresentar
+    
     const conteudo = (
         <div>
-            <span>&copy; 2021 - Refugiados1 - <a href="Home/Sobre" asp-area="" asp-controller="Home" asp-action="Sobre">Sobre</a></span>
-            <h3>Tempo atual</h3>
             <p>Estamos na seguinte hora: {new Date().toLocaleTimeString()}</p>
         </div>
     )
@@ -15,7 +13,7 @@ const root = document.getElementById('root')
 
 root.style.textAlign = "center"
 
-root.style.marginTop = "30px"
+root.style.marginTop = "10px"
 
 
 
